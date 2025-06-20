24cdbd26dd84f090eb4bfb7a7e0c9219c04583e9
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-20 23:07:53 +0600

    alias type learning

---
+++ src/Alias.ts
@@ -0,0 +1,48 @@
+{
+  //type alias
+  type Student = {
+    name: string;
+    age: number;
+    contactNo: string;
+    gender: string;
+    address: string;
+  };
+
+  const student1: {
+    name: string;
+    age: number;
+    gender: string;
+    contactNo: string;
+    address: string;
+  } = {
+    name: "Tanim Bin Aziz",
+    age: 24,
+    gender: "male",
+    contactNo: "01719000060",
+    address: "Bangladesh",
+  };
+
+  const student2: {
+    name: string;
+    age: number;
+    gender: string;
+    address: string;
+  } = {
+    name: "chomok",
+    age: 25,
+    gender: "male",
+    address: "bangladesh",
+  };
+
+  const student3: {
+    name: string;
+    age: number;
+    gender: string;
+    address: string;
+  } = {
+    name: "shipon",
+    age: 23,
+    gender: "male",
+    address: "bangladesh",
+  };
+}
