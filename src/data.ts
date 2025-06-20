8ebfd0c78ceb63e8290a0287cbab76d6c65d1505
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-20 19:13:53 +0600

    data type learning

---
+++ src/data.ts
@@ -0,0 +1,14 @@
+// string type data
+let firstName: string = "Tanim Bin Aziz";
+// number type data
+let roll: number = 123;
+//boolean type data
+let isAdmin: boolean = true;
+// undefine data
+let x: undefined = undefined;
+// null data
+let y: null = null;
+
+// array type
+// string type array cannot be used as number or any other type when you set :string
+let friends: string[] = ["tanim", "chomok", "sagor"];
