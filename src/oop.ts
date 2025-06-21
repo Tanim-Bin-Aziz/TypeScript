e4a0be27b70828ecd9b8eaefe0aec4c92fcc559e
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2025-06-21 14:00:14 +0600

    parameter properties learned with public class

--- src/oop.ts
+++ src/oop.ts
@@ -1,14 +1,19 @@
 {
   //
   class Animal {
-    name: string;
-    species: string;
-    sound: string;
+    // name: string;
+    // species: string;
+    // sound: string;
+    //parameter properties
 
-    constructor(name: string, species: string, sound: string) {
-      this.name = name;
-      this.species = species;
-      this.sound = sound;
+    constructor(
+      public name: string,
+      public species: string,
+      public sound: string
+    ) {
+      //   this.name = name;
+      //   this.species = species;
+      //   this.sound = sound;
     }
     makeSound() {
       console.log(`The ${this.name} says ${this.sound}`);
@@ -17,5 +22,6 @@
   const dog = new Animal("German Sherpard Bhai", "Dog", "Ghew Ghew");
   const cat = new Animal("maow", "cat", "meow meow");
   cat.makeSound();
+  dog.makeSound();
   ///
 }
